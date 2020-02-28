0c2af26ef640ad9923b721d579968cc618f98c9e
Alisha Sehgal <alishaseh@gmail.com>
2020-02-27 23:16:28 -0800

    finished the lab!

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'kRo4cbHm8hY8iVVwxul8JBGxUfO7Y1Ar';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [37.7749, -122.4194],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12
+	});
+
+	// Add a marker to the app
+	L.marker([32.88, -117.236]).addTo(map);
 }
